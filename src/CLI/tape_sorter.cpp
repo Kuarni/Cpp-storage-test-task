bb1e1d420babbb5824d5b89296ac17085fec6592
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-05-21 01:04:11 +0300

    feat: add simple CLI

---
+++ src/CLI/tape_sorter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "TapeEmulator.h"
+#include "BubbleTapeSorter.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cout << "Invalid arguments\n" << "Usage: tape_sorter input_tape output_tape" << std::endl;
+    }
+
+    TapeEmulator tape(argv[1], "..");
+
+    (BubbleTapeSorter(tape))();
+}
