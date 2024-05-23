657f6cc92b014bbfeb61a16a911227dd2587b95e
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-05-23 04:34:18 +0300

    feat: add delays

--- src/tapeSorterLib/TapeEmulator.cpp
+++ src/tapeSorterLib/TapeEmulator.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cassert>
 #include <filesystem>
+#include <thread>
 
 #define MIN_M 8
 #define LOAD_INTERVAL ((float) 1/4)
@@ -11,7 +12,9 @@
 #define TMP_PREFIX "tmp"
 
 TapeEmulator::TapeEmulator(const std::string &input_tape_path, std::string output_tape_path, uint64_t m,
+                           std::chrono::nanoseconds read_write_delay, std::chrono::nanoseconds move_delay,
                            std::string tmp_tapes_location) :
+        read_write_delay(read_write_delay), move_delay(move_delay),
         input_tape_path(input_tape_path),
         output_tape_path(std::move(output_tape_path)),
         max_number_of_elements(m / sizeof(int32_t) / 2),
@@ -33,8 +36,9 @@ TapeEmulator::TapeEmulator(const std::string &input_tape_path, std::string outpu
         if (!cur) {
             throw std::system_error{errno, std::generic_category(), get_tmp_file_name(tmp_file_counter - 1)};
         }
-        for (i = 0; i < max_number_of_elements && in >> input; i++)
+        for (i = 0; i < max_number_of_elements && in >> input; i++) {
             cur << input << " ";
+        }
     }
     last_tape_num = tmp_file_counter - 1;
     last_tape_size = i;
@@ -45,20 +49,20 @@ TapeEmulator::TapeEmulator(const std::string &input_tape_path, std::string outpu
     load_tmp_tape(cur_tape, cur_tape.num);
 }
 
-void TapeEmulator::load_tmp_tape(tape_pointer &tmp_tape, uint32_t tape_num) {
+void TapeEmulator::load_tmp_tape(tmp_tape &tmp_tape, uint32_t tape_num) {
     std::ifstream in(get_tmp_file_name(tape_num));
-    tmp_tape.p = new int32_t[max_number_of_elements];
+    tmp_tape.array = new int32_t[max_number_of_elements];
     tmp_tape.num = tape_num;
-    for (uint64_t i = 0; in >> tmp_tape.p[i]; i++);
+    for (uint64_t i = 0; in >> tmp_tape.array[i]; i++);
 }
 
-void TapeEmulator::save_tmp_tape(TapeEmulator::tape_pointer &tmp_tape) {
-    if (!tmp_tape.p)
+void TapeEmulator::save_tmp_tape(TapeEmulator::tmp_tape &tmp_tape) {
+    if (!tmp_tape.array)
         return;
     std::ofstream out(get_tmp_file_name(tmp_tape.num));
     for (uint64_t i = 0; i < (tmp_tape.num == last_tape_num ? last_tape_size : max_number_of_elements); i++)
-        out << tmp_tape.p[i] << " ";
-    delete[] tmp_tape.p;
+        out << tmp_tape.array[i] << " ";
+    delete[] tmp_tape.array;
 }
 
 std::string TapeEmulator::get_tmp_file_name(uint32_t tape_num) {
@@ -66,14 +70,17 @@ std::string TapeEmulator::get_tmp_file_name(uint32_t tape_num) {
 }
 
 void TapeEmulator::write(int32_t value) {
-    cur_tape.p[relative_position] = value;
+    std::this_thread::sleep_for(read_write_delay);
+    cur_tape.array[relative_position] = value;
 }
 
 int32_t TapeEmulator::read() {
-    return cur_tape.p[relative_position];
+    std::this_thread::sleep_for(read_write_delay);
+    return cur_tape.array[relative_position];
 }
 
 bool TapeEmulator::moveLeft() {
+    std::this_thread::sleep_for(move_delay);
     if (cur_tape.num == 0) {
         if (relative_position == 0) {
             return false;
@@ -83,14 +90,14 @@ bool TapeEmulator::moveLeft() {
     }
 
     if (relative_position <= (uint64_t) (max_number_of_elements * LOAD_INTERVAL) &&
-        (next_tape.num != cur_tape.num - 1 || !next_tape.p)) {
+        (next_tape.num != cur_tape.num - 1 || !next_tape.array)) {
         assert(cur_tape.num != 0);
         save_tmp_tape(next_tape);
         load_tmp_tape(next_tape, cur_tape.num - 1);
     }
 
     if (relative_position-- == 0) {
-        assert(next_tape.p);
+        assert(next_tape.array);
         assert(next_tape.num == cur_tape.num - 1);
         std::swap(cur_tape, next_tape);
         relative_position = max_number_of_elements - 1;
@@ -100,6 +107,7 @@ bool TapeEmulator::moveLeft() {
 }
 
 bool TapeEmulator::moveRight() {
+    std::this_thread::sleep_for(move_delay);
     if (cur_tape.num == last_tape_num) {
         if (relative_position + 1 < last_tape_size) {
             relative_position++;
@@ -109,14 +117,14 @@ bool TapeEmulator::moveRight() {
     }
 
     if (relative_position >= (uint64_t) (max_number_of_elements * (1 - LOAD_INTERVAL)) &&
-        (next_tape.num != cur_tape.num + 1 || !next_tape.p)) {
+        (next_tape.num != cur_tape.num + 1 || !next_tape.array)) {
         assert(cur_tape.num != last_tape_num);
         save_tmp_tape(next_tape);
         load_tmp_tape(next_tape, cur_tape.num + 1);
     }
 
     if (++relative_position == max_number_of_elements) {
-        assert(next_tape.p);
+        assert(next_tape.array);
         assert(next_tape.num == cur_tape.num + 1);
         std::swap(cur_tape, next_tape);
         relative_position = 0;
--- src/tapeSorterLib/TapeEmulator.h
+++ src/tapeSorterLib/TapeEmulator.h
@@ -2,31 +2,37 @@
 #define TAPESORTER_TAPEEMULATOR_H
 
 #include <vector>
+#include <chrono>
 #include "Tape.h"
 
 class TapeEmulator : public Tape {
 private:
+    const std::chrono::nanoseconds read_write_delay;
+    const std::chrono::nanoseconds move_delay;
     const uint64_t max_number_of_elements;
     uint64_t relative_position;
     std::string input_tape_path;
     std::string output_tape_path;
     std::string tmp_tapes_location;
-    struct tape_pointer {
-        int32_t *p;
+    struct tmp_tape {
+        int32_t *array;
         uint32_t num;
     };
-    tape_pointer cur_tape;
-    tape_pointer next_tape;
+    tmp_tape cur_tape;
+    tmp_tape next_tape;
     uint64_t last_tape_size;
     uint32_t last_tape_num;
 
-    void load_tmp_tape(tape_pointer &tmp_tape, uint32_t tape_num);
+    void load_tmp_tape(tmp_tape &tmp_tape, uint32_t tape_num);
 
-    void save_tmp_tape(TapeEmulator::tape_pointer &tmp_tape);
+    void save_tmp_tape(TapeEmulator::tmp_tape &tmp_tape);
 
     inline std::string get_tmp_file_name(uint32_t tape_num);
+
 public:
     TapeEmulator(const std::string &input_tape_path, std::string output_tape_path, uint64_t m,
+                 std::chrono::nanoseconds read_write_delay = std::chrono::nanoseconds(0),
+                 std::chrono::nanoseconds move_delay = std::chrono::nanoseconds(0),
                  std::string tmp_tapes_location = ".");
 
     void write(int32_t) override;
